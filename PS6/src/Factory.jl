2fca977c787be6272c7b58511cb474aee31f6a3c
anthony1234p <119891367+anthony1234p@users.noreply.github.com>
2023-04-11 17:43:24 -0400

    new

--- PS6/src/Factory.jl
+++ PS6/src/Factory.jl
@@ -27,7 +27,12 @@ function _build_metabolite_id_array(data::Dict{String,Any})::Array{String,1}
     metabolite_id_array = Array{String,1}()
 
     # TODO: fill the metabolite_id_array with the metabolite id's from the data dictionary
-    # ....
+    metabolites = data["metabolites"];
+    for metabolite in metabolites
+        id_value = metabolite["id"]
+        push!(metabolite_id_array, id_value);
+    end
+
 
     # return -
     return metabolite_id_array;
@@ -42,7 +47,13 @@ function _build_reaction_id_array(data::Dict{String,Any})::Array{String,1}
     reaction_id_array = Array{String,1}()
 
     # TODO: fill the reaction_id_array with the reaction id's from the data dictionary
-    # ....
+   reactions = data["reactions"]
+    for reaction in reactions
+        id_value = reaction["id"]
+        push!(reaction_id_array, id_value)
+    end
+
+        
 
     # return -
     return reaction_id_array;
@@ -59,7 +70,13 @@ function _build_bounds_array(data::Dict{String,Any})::Array{Float64,2}
     bounds_array = Array{Float64,2}(undef,number_of_reactions,2)
 
     # TODO: fill in the entries of the bounds array, first col is the lower bound, second col is the upper bound
-    # ...
+    # bounds is going to be a number of reactions by 2
+    
+    for i in 1:number_of_reactions
+        reaction = list_of
+
+
+    #each row is a reaction
 
     # return -
     return bounds_array
